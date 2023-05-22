475b1cc0ab6fba99523a1dbccbdd3144b127b5b3
ktroisn <brouillonquentin@gmail.com>
2023-05-22 17:03:10 +0200

    code readability

--- Exercise_3/main.js
+++ Exercise_3/main.js
@@ -1,42 +1,59 @@
 // Création de la balise div, variable cell dans le js
 let cell = document.createElement('div');
+
 // Ajout de la class cell a la variable cell
 cell.classList.add('cell');
+
 // Définition du symbole du joueur 1
 const playerOne = 'X';
+
 // Définition du symbole du joueur 2
 const playerTwo = 'O';
+
 // Définition de la variable gameStatus comme étant l'element portant l'id gameStatus dans index.html
 const gameStatus = document.getElementById('gameStatus');
+
 // Par défaut c'est le joueur un qui commence (par défaut c'est le tour du joueur un)
 let playerTurn = playerOne;
+
 // Définir la variable grid, contenant l'element aillant pour id grid dans index.html
 let grid = document.querySelector('#grid');
 
+
 // Boucle permettant de créer 9 clone de box
 for(let i = 1; i <= 9; i++){
+
     // Définir la variable newcell contenant le clone
     let newcell = cell.cloneNode();
+
     // Ajouter a l'interieur de la div grid, la newcell (le clone de cell qui vient detre crée)
     grid.appendChild(newcell);
+
     // Ajoute un evenement a newcell, au click lancer la fonction playGame, on peut cliquer une seule fois sur chaque newcell
     newcell.addEventListener('click', playGame, { once: true });
 }
 
-// Fonction permettant d'ecrire les symboes de chaque joueur
+
+// Fonction permettant d'ecrire les symboles de chaque joueur
 function playGame(e){
+    
     // Ajouter le symbole qui est actuellement dans playerTurn
     e.target.innerHTML = playerTurn;
     
-        // Appel a la fonction updateMessage avec en paramettre playerTurn qui renverra selon le cas, un message de statuts
-        updateMessage(playerTurn); 
-    
+    // Appel a la fonction updateMessage avec en paramettre playerTurn qui renverra selon le cas, un message de statuts
+    updateMessage(playerTurn);
+
     // Quand un joueur joue c'est le tours de l'autre joueur
     playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
+    
 }
+
+
+
 
 // Message de statuts du jeu
 function updateMessage(status){
+
     // Variable utilisé pour stocker les messages
     let statusText;
 
@@ -49,7 +66,13 @@ function updateMessage(status){
         case 'O' : 
             statusText = "Au tour de (X)";
             break;
+        case 'end' : 
+            statusText = "Fin de la partie, actualisez la page pour recommencer.";
+            break;
     }
+
+
     // Remplacement du text de l'element portant l'id gameStatus dans index.html par le text de statusText
     gameStatus.innerHTML = statusText;
+
 }
